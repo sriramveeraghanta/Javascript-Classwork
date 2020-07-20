471802139073b74a7de8540225eb845b69dfecfd
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2020-07-20 14:12:27 -0400

    Delegate

--- events/delegate/js/delegation.js
+++ events/delegate/js/delegation.js
@@ -1,7 +1,20 @@
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-    console.log("hello");
+    let selectedItem = "";
+    const listContainer = document.getElementById("listContainer");
+    for (let i = 0; i <= 10; i++) {
+      const listItem = document.createElement("li");
+      listItem.setAttribute("id", `product_no_${i}`);
+      listItem.innerHTML = `Product number: ${i}`;
+      listItem.addEventListener("click", (event) => {
+        selectedItem = event.target.innerHTML;
+        const outputContainer = document.getElementById("output");
+        outputContainer.innerHTML = `${selectedItem}
+        (ID = ${event.target.getAttribute("id")})`;
+      });
+      listContainer.appendChild(listItem);
+    }
   },
   false
 );
